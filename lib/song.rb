bb12f95b4932d1a060e9de13e24756a21f4dbbe9
Ming Duan <11203duan@gmail.com>
2019-03-25 01:59:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,16 @@ class Song
 
 attr_accessor :name, :artist, :genre
 
+@@all = []
 
 def initialize(name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
+end
+
+def self.all
+
 end
 
 
